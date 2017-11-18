1c3000f6555014c7fef778b2b2d336ab6ddcfe83
jindodog89 <jindodog89@gmail.com>
2017-11-18 03:53:16 -0800

    Update CastingOffice.java

--- CastingOffice.java
+++ CastingOffice.java
@@ -11,16 +11,16 @@ public class CastingOffice {
     boolean rehearseMarker = false;
     Rectangle officeArea;
     ArrayList<String> neighbors = new ArrayList<String>();
-    //Player player;
+    Player player;
 
-    // public CastingOffice(int money, int fame, String whichPlayer, int rank, boolean rehearseMarker){
-    //     this.money = money;
-    //     this.fame = fame;
-    //     this.whichPlayer = whichPlayer;
-    //     this.rank = rank;
-    //     this.rehearseMarker = rehearseMarker;
-    //     //this.player = new player(0,0,0,"","",false);
-    // }
+    public CastingOffice(){
+        // this.money = money;
+        // this.fame = fame;
+        // this.whichPlayer = whichPlayer;
+        // this.rank = rank;
+        // this.rehearseMarker = rehearseMarker;
+        //this.player = new player(0,0,0,"","",false);
+    }
 
     public boolean checkPlayerRank() {
         if(this.rank >=1 && this.rank <=6){
@@ -58,85 +58,118 @@ public class CastingOffice {
         return this.fame;
     }
 
-    public void upgradeRankWithFame() {
-
-        switch(this.rank){
-            case 1:
-                if(this.fame >=5) {
-                    takeFame(5);
-                    setRank(2);
-                }
-                break;
+    public static boolean upgradeRankWithFame(Player currentPlayer, int level) {
+        boolean returnValue = false;
+        switch(level) {
             case 2:
-                if(this.fame >=10) {
-                    takeFame(10);
-                    setRank(3);
+                if(currentPlayer.getFame() >=5) {
+                    currentPlayer.setFame( currentPlayer.getFame() - 5 );
+                    currentPlayer.setRank(2);
+                    returnValue = true;
+                } else {
+                    System.out.println("To rank up to 2, you need at least 5 fames");
+                    returnValue = false;
                 }
                 break;
             case 3:
-                if(this.fame >=15){
-                    takeFame(15);
-                    setRank(4);
+                if(currentPlayer.getFame() >=10){
+                    currentPlayer.setFame( currentPlayer.getFame() - 10 );
+                    currentPlayer.setRank(3);
+                    returnValue = true;
+                } else{
+                    System.out.println("To rank up to 3, you need at least 10 fames");
+                    returnValue = false;
                 }
                 break;
             case 4:
-                if(this.fame >=20){
-                    takeFame(20);
-                    setRank(5);
+                if(currentPlayer.getFame() >=15){
+                    currentPlayer.setFame( currentPlayer.getFame() - 15 );
+                    currentPlayer.setRank(4);
+                    returnValue = true;
+                } else {
+                    System.out.println("To rank up to 4, you need at least 15 fames");
+                    returnValue = false;
                 }
                 break;
             case 5:
-                if(this.fame >=25){
-                    takeFame(25);
-                    setRank(6);
+                if(currentPlayer.getFame() >=20){
+                    currentPlayer.setFame( currentPlayer.getFame() - 20 );
+                    currentPlayer.setRank(5);
+                    returnValue = true;
+                } else {
+                    System.out.println("To rank up to 5, you need at least 20 fames");
+                    returnValue = false;
+                }
+                break;
+            case 6:
+                if(currentPlayer.getFame() >=25){
+                    currentPlayer.setFame( currentPlayer.getFame() - 25 );
+                    currentPlayer.setRank(5);
+                    returnValue = true;
+                } else {
+                    System.out.println("To rank up to 6, you need at least 25 fames");
+                    returnValue = false;
                 }
                 break;
         }
+        return returnValue;
     }
 
-    public void upgradeRankWithMoney() {
-        switch(this.rank){
-            case 1:
-                if(this.money >= 4){
-                    takeMoney(4);
-                    setRank(2);
-                } else{
-                    System.out.println("To rank up to 2, you need at least 4 dollars");
-                }
-                break;
+    public static boolean upgradeRankWithMoney(Player currentPlayer, int level) {
+        boolean returnValue = false;
+        switch(level){
             case 2:
-                if(this.money >= 10){
-                    takeMoney(10);
-                    setRank(3);
+                if(currentPlayer.getMoney() >= 4){
+                    currentPlayer.setMoney( currentPlayer.getMoney() - 4 );
+                    currentPlayer.setRank(4);
+                    returnValue = true;
                 }else{
-                    System.out.println("To rank up to 3, you need at least 10 dollars");
+                    System.out.println("To rank up to 2, you need at least 4 dollars");
+                    returnValue = false;
                 }
                 break;
             case 3:
-                if(this.money >= 18){
-                    takeMoney(18);
-                    setRank(4);
+                if(currentPlayer.getMoney() >= 10){
+                    currentPlayer.setMoney( currentPlayer.getMoney() - 10 );
+                    currentPlayer.setRank(3);
+                    returnValue = true;
                 }else{
-                    System.out.println("To rank up to 4, you need at least 18 dollars");
+                    System.out.println("To rank up to 3, you need at least 10 dollars");
+                    returnValue = false;
                 }
                 break;
             case 4:
-                if(this.money >= 28){
-                    takeMoney(28);
-                    setRank(5);
+                if(currentPlayer.getMoney() >= 18){
+                    currentPlayer.setMoney( currentPlayer.getMoney() - 18 );
+                    currentPlayer.setRank(4);
+                    returnValue = true;
                 }else{
-                    System.out.println("To rank up to 5, you need at least 28 dollars");
+                    System.out.println("To rank up to 4, you need at least 18 dollars");
+                    returnValue = false;
                 }
                 break;
             case 5:
-                if(this.money >= 40) {
-                    takeMoney(40);
-                    setRank(6);
+                if(currentPlayer.getMoney() >= 28) {
+                    currentPlayer.setMoney( currentPlayer.getMoney() - 28 );
+                    currentPlayer.setRank(5);
+                    returnValue = true;
+                }else{
+                    System.out.println("To rank up to 5, you need at least 28 dollars");
+                    returnValue = false;
+                }
+                break;
+            case 6:
+                if(currentPlayer.getMoney() >= 40) {
+                    currentPlayer.setMoney( currentPlayer.getMoney() - 40 );
+                    currentPlayer.setRank(6);
+                    returnValue = true;
                 }else{
                     System.out.println("To rank up to 6, you need at least 40 dollars");
+                    returnValue = false;
                 }
                 break;
         }
+        return returnValue;
     }
 
     public void takeMoney(int subtractMoney) {
@@ -158,10 +191,12 @@ public class CastingOffice {
         String answer = "";
         while(!getOutOfLoop){
             Scanner input = new Scanner(System.in);
-            System.out.println("If you want to upgrade, type in y. Else if you don't want, type in n\n");
+            System.out.println("If you want to upgrade, type in y. Else if you don't want, type in n");
             answer = input.next();
-            if(!answer.toLowerCase().equals("y") || !answer.toLowerCase().equals("n")){
-                System.out.println("Type in y or n. I ain't got no time to mess with you");
+            System.out.println("answer: " + answer);
+
+            if(!answer.toLowerCase().equals("y") && !answer.toLowerCase().equals("n")){
+                System.out.println("Just type in y or n.");
             } else {
                 getOutOfLoop = true;
             }
@@ -178,17 +213,33 @@ public class CastingOffice {
     public String howToUpgrade() {
         boolean getOutOfLoop = false;
         String fameOrDollar = "";
-        System.out.println("Would you like to use fame or dollar to rank up? Type in f or d.\n");
+        System.out.println("Would you like to use fame or dollar to rank up? Type in f or d.");
         while(!getOutOfLoop){
             Scanner input = new Scanner(System.in);
             fameOrDollar = input.next();
-            if(!fameOrDollar.toLowerCase().equals("f") || !fameOrDollar.toLowerCase().equals("d")){
+            if(!fameOrDollar.toLowerCase().equals("f") && !fameOrDollar.toLowerCase().equals("d")){
                 System.out.println("Type in f or d. I ain't got no time to mess with you");
             } else {
                 getOutOfLoop = true;
             }
         }
         return fameOrDollar;
+    }
+
+    public int whatRank(){
+        boolean getOutOfLoop = false;
+        int whatRank = 0;
+        System.out.println("What rank would you like to upgrade to?");
+        while(!getOutOfLoop){
+            Scanner input = new Scanner(System.in);
+            whatRank = input.nextInt();
+            if(whatRank >1 && whatRank < 6){
+                getOutOfLoop = true;
+            } else {
+                System.out.println("Type in a number between 1 and 6");
+            }
+        }
+        return whatRank;
     }
 
 }
