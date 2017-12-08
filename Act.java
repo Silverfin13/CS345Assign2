bbd100bf642f29b6b93937dd6327a1a97bc14162
Emily Wright <emily.wright005@gmail.com>
2017-12-07 22:12:18 -0800

    Add files via upload

--- Act.java
+++ Act.java
@@ -28,28 +28,28 @@ public class Act{
         */
         String playerRole = currentPlayer.getRole();
         if (playerRole.equals("")) {
-            BoardLayersListener.displayGenericMessage("Player not on a role");
+          BoardLayersListener.displayGenericMessage("Player not on a role");
         } else {
-            ParseFile pf = new ParseFile();
-            ArrayList<Card> cards = pf.cards;
-            HashMap<String,Room> rooms = pf.rooms;
-            // find out if a player is working on a card
-            // if yes - return the card
-            // if no - return card with name fail
-            Card card = findCard(cards, playerRole);
-            if (!(card.getCardName().equals("fail"))) {
-                int movieBudget = card.getBudget();
-                Dice dice = new Dice();
-                int diceVal = dice.actRollDice();
-                System.out.printf("You rolled %d", diceVal);
-                actOnCard(currentPlayer, rooms, cards, currentPlayer.getPlayerPosition(), diceVal, movieBudget);
-            } else {
-                int movieBudget = rooms.get(currentPlayer.getPlayerPosition()).getCard().getBudget();
-                Dice dice = new Dice();
-                int diceVal = dice.actRollDice();
-                System.out.printf("You rolled %d.\n", diceVal);
-                actOffCard(currentPlayer, rooms, cards, currentPlayer.getPlayerPosition(), diceVal, movieBudget);
-            }
+          ParseFile pf = new ParseFile();
+          ArrayList<Card> cards = pf.cards;
+          HashMap<String,Room> rooms = pf.rooms;
+          // find out if a player is working on a card
+          // if yes - return the card
+          // if no - return card with name fail
+          Card card = findCard(cards, playerRole);
+          if (!(card.getCardName().equals("fail"))) {
+              int movieBudget = card.getBudget();
+              Dice dice = new Dice();
+              int diceVal = dice.actRollDice();
+              System.out.printf("You rolled %d", diceVal);
+              actOnCard(currentPlayer, rooms, cards, currentPlayer.getPlayerPosition(), diceVal, movieBudget);
+          } else {
+              int movieBudget = rooms.get(currentPlayer.getPlayerPosition()).getCard().getBudget();
+              Dice dice = new Dice();
+              int diceVal = dice.actRollDice();
+              System.out.printf("You rolled %d.\n", diceVal);
+              actOffCard(currentPlayer, rooms, cards, currentPlayer.getPlayerPosition(), diceVal, movieBudget);
+          }
         }
         /* Current player is in casting office */
         if(currentPlayer.getPlayerPosition().equals("CastingOffice")) {
@@ -72,15 +72,15 @@ public class Act{
     }
 
     public Card findCard(ArrayList<Card> cards, String cardName){
-        for (int i = 0; i < cards.size(); i++) {
-            Card currCard = cards.get(i);
-            if (currCard.getCardName().equals(cardName)){
-                return currCard;
-            }
+      for (int i = 0; i < cards.size(); i++) {
+        Card currCard = cards.get(i);
+        if (currCard.getCardName().equals(cardName)){
+          return currCard;
         }
-        Card cardFailure = new Card();
-        cardFailure.setCardName("fail");
-        return cardFailure;
+      }
+      Card cardFailure = new Card();
+      cardFailure.setCardName("fail");
+      return cardFailure;
     }
 
     public void actOnCard(Player currentPlayer, HashMap<String,Room> rooms, ArrayList<Card> cards, String room, int dieValue, int budgetMovie) {
@@ -95,7 +95,7 @@ public class Act{
                 take--;
                 currRoom.setNumofTakes(take);
                 if (take == 0){
-                    endScene(currRoom, cards, budgetMovie);
+                  endScene(currRoom, cards, budgetMovie);
                 }
                 currentPlayer.setTurn(false);
             } else {
@@ -134,192 +134,192 @@ public class Act{
     }
 
     public boolean takeUpRole(Player currentPlayer, String[] destination){
-        String currentPosition = currentPlayer.getPlayerPosition();
-        ParseFile pf = new ParseFile();
-        ArrayList<Card> cards = pf.cards;
-        HashMap<String,Room> rooms = pf.rooms;
-        if (Objects.equals(currentPlayer.getRole(), "")) {
-            // one word
-            if (destination.length == 2) {
-                // check room
-                Room currRoom = rooms.get(currentPosition);
-                Card card = currRoom.getCard();
-                System.out.println(card.getCardName());
-                ArrayList<partExtra> parts = currRoom.getParts();
-                ArrayList<part> cardParts = card.getCardParts();
-                if (checkRole(currentPlayer, parts, cardParts, destination[1], currRoom)){
-                    return true;
-                } else {
-                    return false;
-                }
-                // multiple words
-            } else {
-                String newDestination = "";
-                for (int l = 1; l < destination.length; l++) {
-                    newDestination = newDestination + " " + destination[l];
-                }
-                newDestination = newDestination.trim();
-                System.out.printf("newDestination: ", newDestination);
-                Room currRoom = rooms.get(currentPosition);
-                Card card = currRoom.getCard();
-                ArrayList<partExtra> parts = currRoom.getParts();
-                ArrayList<part> cardParts = card.getCardParts();
-                if (checkRole(currentPlayer, parts, cardParts, newDestination, currRoom)){
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        } else {
-            BoardLayersListener.displayGenericMessage("Player already in a role.\n");
+      String currentPosition = currentPlayer.getPlayerPosition();
+      ParseFile pf = new ParseFile();
+      ArrayList<Card> cards = pf.cards;
+      HashMap<String,Room> rooms = pf.rooms;
+      if (Objects.equals(currentPlayer.getRole(), "")) {
+        // one word
+        if (destination.length == 2) {
+          // check room
+          Room currRoom = rooms.get(currentPosition);
+          Card card = currRoom.getCard();
+          System.out.println(card.getCardName());
+          ArrayList<partExtra> parts = currRoom.getParts();
+          ArrayList<part> cardParts = card.getCardParts();
+          if (checkRole(currentPlayer, parts, cardParts, destination[1], currRoom)){
+            return true;
+          } else {
             return false;
+          }
+          // multiple words
+        } else {
+          String newDestination = "";
+          for (int l = 1; l < destination.length; l++) {
+            newDestination = newDestination + " " + destination[l];
+          }
+          newDestination = newDestination.trim();
+          System.out.printf("newDestination: ", newDestination);
+          Room currRoom = rooms.get(currentPosition);
+          Card card = currRoom.getCard();
+          ArrayList<partExtra> parts = currRoom.getParts();
+          ArrayList<part> cardParts = card.getCardParts();
+          if (checkRole(currentPlayer, parts, cardParts, newDestination, currRoom)){
+            return true;
+          } else {
+            return false;
+          }
         }
+      } else {
+        BoardLayersListener.displayGenericMessage("Player already in a role.\n");
+        return false;
+      }
     }
 
     public boolean checkRole(Player currentPlayer, ArrayList<partExtra> parts, ArrayList<part> cardParts, String partName, Room room){
-        boolean check = false;
-        for (int i = 0; i < parts.size(); i++) {
-            partExtra currPart = parts.get(i);
-            String name = currPart.getPartName();
-            boolean taken = currPart.getTaken();
-            if (taken != true) {
-                if (name.equals(partName)) {
-                    if (currentPlayer.getRank() >= currPart.getLevel()) {
-                        currentPlayer.setRole(partName);
-                        currentPlayer.setRoleLevel(currPart.getLevel());
-                        currentPlayer.setRoleValue("off");
-                        currPart.setTaken(true);
-                        System.out.printf("\nPlayer %s is now acting in %s. \n", currentPlayer.getPlayer(), partName);
-                        System.out.println(currPart.getArea());
-                        room.setPlayersOnCard((room.getPlayersOnCard()-1));
-                        BoardLayersListener.removePlayer(currentPlayer);
-                        BoardLayersListener.movePlayer(currentPlayer, currPart.getArea(), room);
-                        currentPlayer.setTurn(false);
-                        return true;
-                    }
-                    BoardLayersListener.displayGenericMessage("You do not have a high enough rank for this role.");
+      boolean check = false;
+      for (int i = 0; i < parts.size(); i++) {
+        partExtra currPart = parts.get(i);
+        String name = currPart.getPartName();
+        boolean taken = currPart.getTaken();
+        if (taken != true) {
+              if (name.equals(partName)) {
+                  if (currentPlayer.getRank() >= currPart.getLevel()) {
+                    currentPlayer.setRole(partName);
+                    currentPlayer.setRoleLevel(currPart.getLevel());
+                    currentPlayer.setRoleValue("off");
+                    currPart.setTaken(true);
+                    System.out.printf("\nPlayer %s is now acting in %s. \n", currentPlayer.getPlayer(), partName);
+                    System.out.println(currPart.getArea());
+                    room.setPlayersOnCard((room.getPlayersOnCard()-1));
+                    BoardLayersListener.removePlayer(currentPlayer);
+                    BoardLayersListener.movePlayer(currentPlayer, currPart.getArea(), room);
+                    currentPlayer.setTurn(false);
+                    return true;
                 }
+                BoardLayersListener.displayGenericMessage("You do not have a high enough rank for this role.");
+              }
             } else {
-                BoardLayersListener.displayGenericMessage("This role is already taken. \n");
-                check = true;
+              BoardLayersListener.displayGenericMessage("This role is already taken. \n");
+              check = true;
             }
-        }
-        // make sure it hasn't already taken a part off the card
-        if (check == false) {
-            for (int j = 0; j < cardParts.size(); j++) {
-                part currPart = cardParts.get(j);
-                String currName = currPart.getName();
-                boolean taken = currPart.getTaken();
-                if (taken != true) {
-                    if (currName.equals(partName)) {
-                        if (currentPlayer.getRank() >= currPart.getLevel()) {
-                            currentPlayer.setRole(partName);
-                            currentPlayer.setRoleLevel(currPart.getLevel());
-                            currentPlayer.setRoleValue("on");
-                            currPart.setTaken(true);
-                            System.out.printf("\nPlayer %s is now acting in %s. \n", currentPlayer.getPlayer(), partName);
-                            room.setPlayersOnCard((room.getPlayersOnCard()-1));
-                            BoardLayersListener.removePlayer(currentPlayer);
-                            BoardLayersListener.onCardMove(currentPlayer, currPart.getArea(), room.getCardArea());
-                            currentPlayer.setTurn(false);
-                            return true;
-                        }
-                        System.out.println("You do not have a high enough rank for this role.");
-                        return false;
-                    }
-                } else {
-                    System.out.println("This role is already taken. \n");
-                }
+      }
+      // make sure it hasn't already taken a part off the card
+      if (check == false) {
+        for (int j = 0; j < cardParts.size(); j++) {
+          part currPart = cardParts.get(j);
+          String currName = currPart.getName();
+          boolean taken = currPart.getTaken();
+          if (taken != true) {
+            if (currName.equals(partName)) {
+                 if (currentPlayer.getRank() >= currPart.getLevel()) {
+                  currentPlayer.setRole(partName);
+                  currentPlayer.setRoleLevel(currPart.getLevel());
+                  currentPlayer.setRoleValue("on");
+                  currPart.setTaken(true);
+                  System.out.printf("\nPlayer %s is now acting in %s. \n", currentPlayer.getPlayer(), partName);
+                  room.setPlayersOnCard((room.getPlayersOnCard()-1));
+                  BoardLayersListener.removePlayer(currentPlayer);
+                  BoardLayersListener.onCardMove(currentPlayer, currPart.getArea(), room.getCardArea());
+                  currentPlayer.setTurn(false);
+                  return true;
+              }
+              System.out.println("You do not have a high enough rank for this role.");
+              return false;
             }
+          } else {
+            System.out.println("This role is already taken. \n");
+          }
         }
-        if (check == false) {
-            System.out.println("The role you want to act in is not on the card or board.\n");
-        }
-        System.out.println("The roles you can take is: ");
-        printPartList(cardParts);
-        printExtraPartList(parts);
+      }
+      if (check == false) {
+        System.out.println("The role you want to act in is not on the card or board.\n");
+      }
+      System.out.println("The roles you can take is: ");
+      printPartList(cardParts);
+      printExtraPartList(parts);
 
-        return false;
+      return false;
     }
 
     public void printPartList(ArrayList<part> parts){
-        System.out.println("\nRoles you can choose on the card: ");
-        for (int i = 0; i < parts.size(); i++) {
-            part part = parts.get(i);
-            System.out.println(part.getName());
-        }
+      System.out.println("\nRoles you can choose on the card: ");
+      for (int i = 0; i < parts.size(); i++) {
+        part part = parts.get(i);
+        System.out.println(part.getName());
+      }
     }
 
     public void printExtraPartList(ArrayList<partExtra> cardParts){
-        System.out.println("\nRoles you can choose off the card: ");
-        for (int i = 0; i < cardParts.size(); i++) {
-            partExtra part = cardParts.get(i);
-            System.out.println(part.getPartName());
-        }
+      System.out.println("\nRoles you can choose off the card: ");
+      for (int i = 0; i < cardParts.size(); i++) {
+        partExtra part = cardParts.get(i);
+        System.out.println(part.getPartName());
+      }
     }
 
     public void endScene(Room room, ArrayList<Card> cards, int budgetMovie){
-        ArrayList<Player> onTheCardPlayers = new ArrayList<Player>();
-        Deadwood d = new Deadwood();
-        int scenesLeft = d.scenesLeft;
-        scenesLeft--;
-        d.setScenesLeft(scenesLeft);
-        Card card = room.getCard();
-        String cardName = card.getCardName();
-        ArrayList<part> parts = card.getCardParts();
-        GameSetup GS = new GameSetup();
-        ArrayList<Player> players = GS.players;
-        for (int i=0; i< players.size() ;i++) {
-            Player currPlayer = players.get(i);
-            if (parts.contains(currPlayer.getRole())){
-                onTheCardPlayers.add(currPlayer);
-            }
+      ArrayList<Player> onTheCardPlayers = new ArrayList<Player>();
+      Deadwood d = new Deadwood();
+      int scenesLeft = d.scenesLeft;
+      scenesLeft--;
+      d.setScenesLeft(scenesLeft);
+      Card card = room.getCard();
+      String cardName = card.getCardName();
+      ArrayList<part> parts = card.getCardParts();
+      GameSetup GS = new GameSetup();
+      ArrayList<Player> players = GS.players;
+      for (int i=0; i< players.size() ;i++) {
+        Player currPlayer = players.get(i);
+        if (parts.contains(currPlayer.getRole())){
+          onTheCardPlayers.add(currPlayer);
         }
-        if (onTheCardPlayers.size() > 0){
-            if (onTheCardPlayers.size() == 1) {
-                Dice die = new Dice();
-                ArrayList<Integer> sortedDice = die.rollDice(budgetMovie);
-            } else {
-                onTheCardPlayers = sortPlayers(onTheCardPlayers, parts);
-                Dice die = new Dice();
-                ArrayList<Integer> sortedDice = die.rollDice(budgetMovie);
-                distributeEarnings(onTheCardPlayers, sortedDice);
-            }
+      }
+      if (onTheCardPlayers.size() > 0){
+        if (onTheCardPlayers.size() == 1) {
+          Dice die = new Dice();
+          ArrayList<Integer> sortedDice = die.rollDice(budgetMovie);
         } else {
-            return;
+          onTheCardPlayers = sortPlayers(onTheCardPlayers, parts);
+          Dice die = new Dice();
+          ArrayList<Integer> sortedDice = die.rollDice(budgetMovie);
+          distributeEarnings(onTheCardPlayers, sortedDice);
         }
+      } else {
+        return;
+      }
     }
 
     public static void distributeEarnings(ArrayList<Player> onTheCardPlayers, ArrayList<Integer> diceValue) {
 
-        boolean playersPaid = false;
-        while(playersPaid = false){
-            int dicePosition = 0;
-            for(int i = 0; i < onTheCardPlayers.size(); i++){
-                if(diceValue.get(dicePosition) != diceValue.size()){
-                    int playerMoney = onTheCardPlayers.get(i).getMoney() + diceValue.get(dicePosition);
-                    onTheCardPlayers.get(i).setMoney(playerMoney);
-                    dicePosition ++;
-                }else{
-                    dicePosition = 0;
-                    i--;
-                }
-            }
-            playersPaid = true;
+      boolean playersPaid = false;
+      while(playersPaid = false){
+        int dicePosition = 0;
+        for(int i = 0; i < onTheCardPlayers.size(); i++){
+          if(diceValue.get(dicePosition) != diceValue.size()){
+            int playerMoney = onTheCardPlayers.get(i).getMoney() + diceValue.get(dicePosition);
+            onTheCardPlayers.get(i).setMoney(playerMoney);
+            dicePosition ++;
+          }else{
+            dicePosition = 0;
+            i--;
+          }
         }
+        playersPaid = true;
+      }
     }
 
     public ArrayList<Player> sortPlayers(ArrayList<Player> players, ArrayList<part> parts){
-        for (int i = 0; i < players.size(); i++) {
-            if (i + 1 < players.size()) {
-                Player currPlayer = players.get(i);
-                Player nextPlayer = players.get(i+1);
-                if (currPlayer.getRoleLevel() < nextPlayer.getRoleLevel()) {
-                    players.set(i, nextPlayer);
-                    players.set(i+1, currPlayer);
-                }
-            }
+      for (int i = 0; i < players.size(); i++) {
+        if (i + 1 < players.size()) {
+          Player currPlayer = players.get(i);
+          Player nextPlayer = players.get(i+1);
+          if (currPlayer.getRoleLevel() < nextPlayer.getRoleLevel()) {
+            players.set(i, nextPlayer);
+            players.set(i+1, currPlayer);
+          }
         }
-        return players;
+      }
+      return players;
     }
 }
