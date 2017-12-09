40708534fe81fad7583556cc035894adba5e987e
Emily Wright <emily.wright005@gmail.com>
2017-12-08 18:12:03 -0800

    Update Board.java

--- Board.java
+++ Board.java
@@ -5,14 +5,16 @@ public class Board {
   public ArrayList<Card> cards = new ArrayList<Card>();
   public ArrayList<Card> cardStack = new ArrayList<Card>();
   public HashMap<String,Room> rooms = new HashMap<String,Room>();
-
+  
+  //  creating the board
   public void setBoard(){
     ParseFile pf = new ParseFile();
     this.cards = pf.cards;
     this.cardStack = pf.cards;
     this.rooms = pf.rooms;
   }
-
+  
+  // placing cards on the board using a stack for cards
   public void placeCards(){
     int i = 0;
     for (String key: rooms.keySet()){
