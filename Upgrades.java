1069de24b1811cd763736d875c04d73625a1a136
jindodog89 <jindodog89@gmail.com>
2017-12-08 18:21:42 -0800

    Update Upgrades.java

--- Upgrades.java
+++ Upgrades.java
@@ -1,13 +1,14 @@
 import java.util.*;
 import java.awt.Rectangle;
 
+/*Upgrades is an object that is used to upgrade user's rank */
 public class Upgrades {
 
   private int level = 0;
   private String currency = "";
   private int amt = 0;
   private Rectangle area;
-
+/*createUpgrades is an contructor that holds player rank, currency and minimum requirements to upgrade  */
   public void createUpgrade(int level, String currency, int amt, Rectangle area) {
     this.level = level;
     this.currency = currency;
