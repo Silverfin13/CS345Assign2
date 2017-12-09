c63cd6e4ef777b92a8ccdb21cc31463ba6682b9a
Emily Wright <emily.wright005@gmail.com>
2017-12-08 18:28:55 -0800

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Dice {
 
-
+    // roll dice for end of scene
     public ArrayList<Integer> rollDice(int numDice) {
         ArrayList<Integer> descionDice = new ArrayList<Integer>();
         int roll;
@@ -16,7 +16,7 @@ public class Dice {
         Collections.sort(descionDice, Collections.reverseOrder());
         return descionDice;
     }
-
+    // roll the dice
     public int actRollDice() {
         int roll = (int)(Math.random()*6) + 1;
         return roll;
